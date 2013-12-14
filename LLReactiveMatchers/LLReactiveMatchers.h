16d9f6f8686d594f2c11753981c7a69bb1c4c592
Lawrence Lomax <lomax.lawrence@gmail.com>
2013-12-14 14:48:03 -0800

    Updated Umbrella header to include matchers

--- LLReactiveMatchers/LLReactiveMatchers.h
+++ LLReactiveMatchers/LLReactiveMatchers.h
@@ -1,8 +1,9 @@
 // Umbrella Header
-
-#import <ReactiveCocoa/ReactiveCocoa.h>
-#import <Specta/Specta.h>
-#import <Expecta/Expecta.h>
-
 #import "LLSignalTestRecorder.h"
-
+#import "EXPMatchers+complete.h"
+#import "EXPMatchers+finish.h"
+#import "EXPMatchers+haveIdenticalErrors.h"
+#import "EXPMatchers+haveIdenticalEvents.h"
+#import "EXPMatchers+haveIdenticalValues.h"
+#import "EXPMatchers+sendError.h"
+#import "EXPMatchers+sendValues.h"
