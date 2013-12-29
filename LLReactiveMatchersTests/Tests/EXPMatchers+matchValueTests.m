fd8f7225742d308786e0c5f5e80bae6d8a566938
Lawrence Lomax <lomax.lawrence@gmail.com>
2013-12-30 09:21:19 +1300

    matchValues  takes recorders

--- LLReactiveMatchersTests/Tests/EXPMatchers+matchValueTests.m
+++ LLReactiveMatchersTests/Tests/EXPMatchers+matchValueTests.m
@@ -9,52 +9,79 @@
 
 - (void) test_nonSignalActual {
     NSArray *actual = @[@1, @2, @3];
+    NSString *failureString = @"expected: actual to be a signal or recorder";
     
     assertFail(test_expect(actual).to.matchValue(0, ^(id value){
         return YES;
-    }), @"expected: actual to be a signal or recorder");
+    }), failureString);
     
     assertFail(test_expect(actual).toNot.matchValue(0, ^(id value){
         return YES;
-    }), @"expected: actual to be a signal or recorder");
+    }), failureString);
 }
 
 - (void) test_emptySignal {
     RACSignal *actual = [RACSignal.empty setNameWithFormat:@"foo"];
+    NSString *failureString = @"expected: actual foo to to match value at index 0, got: only 0 values sent";
     
     assertPass(test_expect(actual).toNot.matchValue(0, ^(id value){
         return NO;
     }));
-    
     assertFail(test_expect(actual).to.matchValue(0, ^(id value){
         return NO;
-    }), @"expected: actual foo to to match value at index 0, got: only 0 values sent");
+    }), failureString);
+    
+    LLSignalTestRecorder *recorder = [LLSignalTestRecorder recordWithSignal:actual];
+    assertPass(test_expect(recorder).toNot.matchValue(0, ^(id value){
+        return NO;
+    }));
+    assertFail(test_expect(recorder).to.matchValue(0, ^(id value){
+        return NO;
+    }), failureString);
 }
 
 - (void) test_matchIndexToDamnHigh {
     RACSignal *actual = [[LLReactiveMatchersFixtures values:@[@0, @1, @2, @3]] setNameWithFormat:@"foo"];
+    NSString *failureString = @"expected: actual foo to to match value at index 10, got: only 4 values sent";
     
     assertPass(test_expect(actual).toNot.matchValue(10, ^(id value){
         return NO;
     }));
-    
     assertFail(test_expect(actual).to.matchValue(10, ^(id value){
         return NO;
-    }), @"expected: actual foo to to match value at index 10, got: only 4 values sent");
+    }), failureString);
+    
+    LLSignalTestRecorder *recorder = [LLSignalTestRecorder recordWithSignal:actual];
+    assertPass(test_expect(recorder).toNot.matchValue(10, ^(id value){
+        return NO;
+    }));
+    assertFail(test_expect(recorder).to.matchValue(10, ^(id value){
+        return NO;
+    }), failureString);
 }
 
 - (void) test_matchPassIndex {
     RACSignal *actual = [[LLReactiveMatchersFixtures values:@[@0, @1, @2, @3]] setNameWithFormat:@"foo"];
+    NSString *failureString = @"expected: actual foo to to not match value at index 2, got: matched value at index 2";
     
     assertPass(test_expect(actual).to.matchValue(2, ^(id value){
         assertEqualObjects(@2, value);
         return YES;
     }));
-    
     assertFail(test_expect(actual).toNot.matchValue(2, ^(id value){
         assertEqualObjects(@2, value);
         return YES;
-    }), @"expected: actual foo to to not match value at index 2, got: matched value at index 2");
+    }), failureString);
+    
+    LLSignalTestRecorder *recorder = [LLSignalTestRecorder recordWithSignal:actual];
+    assertPass(test_expect(recorder).to.matchValue(2, ^(id value){
+        assertEqualObjects(@2, value);
+        return YES;
+    }));
+    assertFail(test_expect(recorder).toNot.matchValue(2, ^(id value){
+        assertEqualObjects(@2, value);
+        return YES;
+    }), failureString);
 }
 
 
