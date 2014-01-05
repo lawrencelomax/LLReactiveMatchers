8368c788fbaaf3f59a2d204378e38b75ef38ac93
Lawrence Lomax <lomax.lawrence@gmail.com>
2014-01-05 17:01:23 +1300

    Subscribe Swizzling

--- LLReactiveMatchers/Matchers/EXPMatchers+beSubscribedTo.m
+++ LLReactiveMatchers/Matchers/EXPMatchers+beSubscribedTo.m
@@ -2,45 +2,54 @@
 
 #import <objc/runtime.h>
 
-EXPMatcherImplementationBegin(beSubscribedTo, (NSInteger times))
+static void *subscriptionCountKey = &subscriptionCountKey;
 
-static void LLSwizzleSubscribe(Class class, (void)(^subscriptionBlock)(RACSignal *signal, id<RACSubscriber> subscriber) ) {
-	SEL subscribeSelector = @selector(subscribe:);
-
-    static RACDisposable *(*originalSubscribeMethod)(id, SEL, id<RACSubscriber>) = NULL;
-    if(originalSubscribeMethod) {
-        Method method = class_getInstanceMethod(class, subscribeSelector);
-        originalSubscribeMethod = (__typeof__(forwardInvocationMethod))method_getImplementation(subscribeMethod);
-    }
-    id replacedImplementation = ^(RACSignal *signal, id<RACSubscriber> subscriber){
-        subscriptionBlock(signal, subscriber);
-        return originalSubscribeMethod(signal, subscribeSelector, subscriber);
-    };
-    
-	class_replaceMethod(class, forwardInvocationSEL, imp_implementationWithBlock(newForwardInvocation), "v@:@");
+static void setSubscriptionCount(RACSignal *signal, NSUInteger count) {
+    objc_setAssociatedObject(signal, subscriptionCountKey, @(count), OBJC_ASSOCIATION_COPY);
 }
+
+static NSUInteger getSubscriptionCount(RACSignal *signal) {
+    NSNumber *subscriptionCount = objc_getAssociatedObject(signal, subscriptionCountKey);
+    return subscriptionCount ? subscriptionCount.integerValue : -1;
+}
+
+static void swizzleSubscribeIfNeeded() {
+    static BOOL hasSwizzledSubscribe = NO;
+
+    @synchronized(RACSignal.class) {
+        if(!hasSwizzledSubscribe) {
+            SEL subscribeSelector = @selector(subscribe:);
+            
+            static RACDisposable *(*originalSubscribeImplementation)(id, SEL, id<RACSubscriber>) = NULL;
+            originalSubscribeImplementation = (typeof(originalSubscribeImplementation)) class_getMethodImplementation(RACSignal.class, subscribeSelector);
+            
+            typeof(originalSubscribeImplementation) newImplementation = (typeof(originalSubscribeImplementation)) imp_implementationWithBlock(^(RACSignal *signal, id<RACSubscriber> subscriber){
+                
+                setSubscriptionCount(signal, getSubscriptionCount(signal) + 1);
+                
+                return originalSubscribeImplementation(signal, subscribeSelector, subscriber);
+            });
+            
+            Method method = class_getInstanceMethod(RACSignal.class, subscribeSelector);
+            method_setImplementation(method, (IMP) newImplementation);
+            
+            hasSwizzledSubscribe = YES;
+        }
+    }
+}
+
+EXPMatcherImplementationBegin(beSubscribedTo, (NSInteger times))
 
 BOOL correctClasses = [actual isKindOfClass:RACSignal.class];
 __block NSInteger subscriptionCount = 0;
 __block BOOL hasSubscribed = NO;
-
-void (^subscribe)(void) = ^{
-    if(!hasSubscribed) {
-        objc_getMeth
-        
-
-        [self.rac_deallocDisposable addDisposable:[injected subscribeCompleted:^{}]];
-        subscriptionCount--; //We just subscribed to it above
-        hasSubscribed = YES;
-    }
-};
 
 prerequisite(^BOOL{
     return correctClasses;
 });
 
 match(^BOOL{
-    subscribe();
+    swizzleSubscribeIfNeeded();
     
     @synchronized(actual) {
         return (times == subscriptionCount);
@@ -49,20 +58,23 @@ match(^BOOL{
 
 failureMessageForTo(^NSString *{
     if(!correctClasses) {
-        return [LLReactiveMatchersMessages actualNotSignal:actual];
+        return [LLReactiveMatchersMessageBuilder actualNotCorrectClass:actual];
     }
     
     @synchronized(actual) {
+        NSInteger subscriptionCount = getSubscriptionCount(actual);
         return [NSString stringWithFormat:@"Signal %@ subscribed to %d times instead of %d", LLDescribeSignal(actual), subscriptionCount, times];
     }
 });
 
 failureMessageForNotTo(^NSString *{
     if(!correctClasses) {
-        return [LLReactiveMatchersMessages actualNotSignal:actual];
+        return [LLReactiveMatchersMessageBuilder actualNotCorrectClass:actual];
     }
     
     @synchronized(actual) {
+        NSInteger subscriptionCount = getSubscriptionCount(actual);
+
         return [NSString stringWithFormat:@"Signal %@ subscribed to %d times", LLDescribeSignal(actual), subscriptionCount];
     }
 });
